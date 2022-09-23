86f6a2ccb9d5a321729acf65210483f5a735d4f2
Suri <109096878+Surisuri90@users.noreply.github.com>
2022-09-23 20:50:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-<body>
-
-    <div id="box" class="#97B350">
-     <img style="width:50%;border:1px solid #F4EFEF;" src="file:///Users/surili/Desktop/timezonemap.png" />
-            <h1>MY FIRST JS</h1>
-     <script src="script.js"></script>     
+   
     <button type="button"
     onclick="document.getElementById('demo').innerHTML = Date()">
     Click me to display Date and Time.</button>
+    <botton type="button1"
+    onclick="document.getElementById('demo').innerHTML = DATE()">
     
     <p id="demo"></p>
     </html>
